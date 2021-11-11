4af8a67a5a90a718bc7dd6f8b47192829a94a7ac
batrung <batrung255@gmail.com>
2021-11-11 10:20:56 +0700

    hoan thanh

--- router/src/component/home.js
+++ router/src/component/home.js
@@ -1,11 +1,15 @@
 import React from "react";
 import Menu from "./menu";
 import '../css/home.css';
+import { Link } from "react-router-dom";
 class home extends React.Component{
     render(){
+        const img = "book.jpg";
         return(
             <div className="nav">
-                <h3><a href="/"> Logo </a> </h3>
+                <h3><Link to="/"> 
+                <img src={require(`../image/${img}`).default} alt="Logo" style={ {width: '3rem' , height: '3rem' } }/>
+                 </Link> </h3>
                 <ul className="nav__link">
                     <li> <Menu name = "HTML"/> </li>
                     <li> <Menu name = "CSS"/> </li>
